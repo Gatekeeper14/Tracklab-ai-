6d1961602b725ff8a8790c78b3c0f4b5f0c4b6e7
Gatekeeper14 <Parapsychologenius@yahoo.com>
2026-04-23 18:48:34 -0400

    Create config.py

---
+++ config.py
@@ -0,0 +1,9 @@
+import os
+
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+
+PAYPAL_CLIENT_ID = os.getenv("PAYPAL_CLIENT_ID")
+PAYPAL_SECRET = os.getenv("PAYPAL_SECRET")
+
+UPLOAD_FOLDER = "uploads"
+OUTPUT_FOLDER = "outputs"
