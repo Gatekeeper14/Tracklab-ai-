ce058819e612348b8ce919fec045713daf578548
Gatekeeper14 <Parapsychologenius@yahoo.com>
2026-04-23 17:45:37 -0400

    Create config.py

---
+++ config.py
@@ -0,0 +1,9 @@
+BOT_TOKEN = "PASTE_YOUR_TELEGRAM_BOT_TOKEN"
+
+PAYPAL_LINK = "https://www.paypal.com/ncp/payment/KB85TDSPUAHBC"
+
+TERMS_URL = "https://Gatekeeper14.github.io/codeclean-legal/terms"
+PRIVACY_URL = "https://Gatekeeper14.github.io/codeclean-legal/privacy"
+REFUND_URL = "https://Gatekeeper14.github.io/codeclean-legal/refund"
+
+FREE_USES = 3
