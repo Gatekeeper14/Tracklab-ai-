7241256fce65fa2d0b4f955da8767839d265ff60
Gatekeeper14 <Parapsychologenius@yahoo.com>
2026-04-25 00:50:35 -0400

    Delete config.py

--- config.py
+++
@@ -1,9 +0,0 @@
-import os
-
-OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
-
-PAYPAL_CLIENT_ID = os.getenv("PAYPAL_CLIENT_ID")
-PAYPAL_SECRET = os.getenv("PAYPAL_SECRET")
-
-UPLOAD_FOLDER = "uploads"
-OUTPUT_FOLDER = "outputs"
